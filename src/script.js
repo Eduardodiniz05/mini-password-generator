7e6457332e463f5496a6d76b79cc3b40d97e6a45
eduardodiniz05 <eduardo.diniz005@gmail.com>
2022-04-06 09:32:35 -0300

    Adicionado lista de para gerar a senha

--- src/script.js
+++ src/script.js
@@ -1,5 +1,19 @@
+const passwords = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+                    0,1,2,3,4,5,6,7,8,9,
+                 '!', '@', ',', '.', '/', '*', '-', "'", '"', ':', '?', "|", "#", '$', '%', '&', '(', ')', '_', '+', '~', '=', '§'];
+
+
+
+
+
+
+
+
+
+                 
 // const container = document.getEelementById('container');
 // const nightModeOn = document.getEelementById('night-mode-on');
 // const nightModeOnContainer = document.getEelementById('night-mode-container');
 // const PasswordViewer = document.getEelementById('password-viewer');
 // const codeGenerator = document.getEelementById('code-generator');
+
