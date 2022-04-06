a9d965bdb1b2548db4786e411f69305973576174
eduardodiniz05 <eduardo.diniz005@gmail.com>
2022-04-06 10:54:58 -0300

    Adicionado inicio de alteração de modo dark para modo light

--- src/script.js
+++ src/script.js
@@ -4,16 +4,35 @@ const passwords = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','
 
 
 
+    
+    
+let changeMode = document.addEventListener('click', function() {
+        this.getElementById('night-mode-on').setAttribute('id','night-mode-off');
+        this.getElementById('night-mode-container').setAttribute('id', 'night-mode-container-off');
+        this.getElementById('container').setAttribute('id','container-off');
+        this.getElementsById('night-mode-off').setAttribute('input', 'off');
+
+
+
+        this.getElementById('night-mode-off').setAttribute('id','night-mode-on');
+        this.getElementById('night-mode-container-off').setAttribute('id', 'night-mode-container');
+        this.getElementById('container-off').setAttribute('id','container');
+    
+        
+    console.log(changeMode)
+});
 
 
 
 
 
 
-                 
-// const container = document.getEelementById('container');
-// const nightModeOn = document.getEelementById('night-mode-on');
-// const nightModeOnContainer = document.getEelementById('night-mode-container');
+
+
+
 // const PasswordViewer = document.getEelementById('password-viewer');
 // const codeGenerator = document.getEelementById('code-generator');
 
+// const nightModeOn = document.getEelementById('night-mode-on').setAttribute(id,'night-mode-off');
+// const nightModeOnContainer = document.getEelementById('night-mode-container').setAttribute(id, 'night-mode-container-off');
+// const container = document.getEelementById('container').setAttribute('id','container-off');
