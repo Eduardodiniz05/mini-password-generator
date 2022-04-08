a87bc02e77fa80e8e4c9b807a23e5f6483a821a9
eduardodiniz05 <eduardo.diniz005@gmail.com>
2022-04-08 16:48:47 -0300

    Adicionado inicio de alteração de modo dark para modo light

--- src/script.js
+++ src/script.js
@@ -2,37 +2,50 @@ const passwords = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','
                     0,1,2,3,4,5,6,7,8,9,
                  '!', '@', ',', '.', '/', '*', '-', "'", '"', ':', '?', "|", "#", '$', '%', '&', '(', ')', '_', '+', '~', '=', '§'];
 
+const randomNumber = documeent.getElementById
+
+
+
+const  generateRandomString = (num) => {
+   const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+   let result1= ' ';
+   const charactersLength = characters.length;
+   for ( let i = 0; i < num; i++ ) {
+       result1 += characters.charAt(Math.floor(Math.random() * charactersLength));
+   }
+
+   return result1;
+}
+
+const displayRandomString = () =>{
+  let randomStringContainer = document.getElementById('random_string'); 
+   randomStringContainer.innerHTML =  generateRandomString(8);    
+}
+
+console.log(generateRandomString(5));
+         
+         
+// let changeMode = document.addEventListener('click', function(){
+//    this.getElementById('night-mode-on').setAttribute('id','night-mode-off');
+//    this.getElementById('night-mode-container').setAttribute('id', 'night-mode-container-off');
+//    this.getElementById('container').setAttribute('id','container-off');
+//    this.getElementsById('night-mode-on').setAttribute('input', 'off');
+// });
+
+
+      
+
+      
+
+
+// 'night-mode-container' = document.addEventListener('click', changeMode)
 
 
     
     
-let changeMode = document.addEventListener('click', function() {
-        this.getElementById('night-mode-on').setAttribute('id','night-mode-off');
-        this.getElementById('night-mode-container').setAttribute('id', 'night-mode-container-off');
-        this.getElementById('container').setAttribute('id','container-off');
-        this.getElementsById('night-mode-off').setAttribute('input', 'off');
-
-
-
-        this.getElementById('night-mode-off').setAttribute('id','night-mode-on');
-        this.getElementById('night-mode-container-off').setAttribute('id', 'night-mode-container');
-        this.getElementById('container-off').setAttribute('id','container');
-    
-        
-    console.log(changeMode)
-});
 
 
 
 
 
 
-
-
-
-// const PasswordViewer = document.getEelementById('password-viewer');
-// const codeGenerator = document.getEelementById('code-generator');
-
-// const nightModeOn = document.getEelementById('night-mode-on').setAttribute(id,'night-mode-off');
-// const nightModeOnContainer = document.getEelementById('night-mode-container').setAttribute(id, 'night-mode-container-off');
-// const container = document.getEelementById('container').setAttribute('id','container-off');
