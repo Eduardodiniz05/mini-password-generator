c7021de361cf272be02dcc40ae8c7ba8d9f9b2fb
eduardodiniz05 <eduardo.diniz005@gmail.com>
2022-04-12 17:27:51 -0300

    mais alterações no css e algumas no js

--- src/script.js
+++ src/script.js
@@ -7,7 +7,29 @@ const passwords = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','
 
     
     
+let spanNumber = document.getElementById('box');
 
+for(i = 3; i < 36; i++) {
+      let span = document.createElement('span');
+      span.textContent = i;
+      spanNumber.appendChild(span);
+}
+
+let num = numbers.getElementsByTagName('span');
+
+let index = 0;
+
+function prevNum() {
+   num[index].style.display = 'none';
+   index = (index - 1 +  num.length) % num.length;
+   num[index].style.display = 'initial';
+}
+
+function nextNum() {
+   num[index].style.display = 'none';
+   index = (index + 1) % num.length;
+   num[index].style.display = 'initial';
+}
 
 
 
