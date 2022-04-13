432c6ce5f09b9b43e895f7f88e8ab06140f8f56a
eduardodiniz05 <eduardo.diniz005@gmail.com>
2022-04-13 17:21:40 -0300

    alteração de teste

--- src/script.js
+++ src/script.js
@@ -2,13 +2,14 @@ const passwords = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','
                     0,1,2,3,4,5,6,7,8,9,
                  '!', '@', ',', '.', '/', '*', '-', "'", '"', ':', '?', "|", "#", '$', '%', '&', '(', ')', '_', '+', '~', '=', '§'];
 
-
-
-
-    
-    
 let spanNumber = document.getElementById('box');
 
+let codeGenerator = document.getElementById('code-generator');
+
+
+
+
+// contador do number
 for(i = 3; i < 36; i++) {
       let span = document.createElement('span');
       span.textContent = i;
@@ -20,18 +21,23 @@ let num = spanNumber.getElementsByTagName('span');
 let index = 0;
 
 
-
-function nextNum() {
-   num[index].style.display = 'none';
-   index = (index + 1) % num.length;
-   num[index].style.display = 'initial';
-}
-
+// Prev number
 function prevNum() {
    num[index].style.display = 'none';
    index = (index - 1 +  num.length) % num.length;
    num[index].style.display = 'initial';
 }
 
+// next number
+function nextNum() {
+   num[index].style.display = 'none';
+   index = (index + 1) % num.length;
+   num[index].style.display = 'initial';
+}
+
+
+if(index >=3 || index <= 33) {
+   codeGenerator.innerHTML()
+}
 
 
